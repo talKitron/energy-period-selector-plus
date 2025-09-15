01f5439ce8c720e95cbf1a54bf33a3422aa9fb50
Tal Kitron <talkitron@gmail.com>
2025-09-15 23:38:49 +0200

    chore: add configuration files and update scripts
    - Introduced .gitattributes to normalize line endings across different environments.
    - Updated package.json scripts for improved linting, testing, and build processes.
    - Added GitHub workflows for automated build, release, and dependency management.
    - Created issue and pull request templates for better contribution guidelines.

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
         }]
     },
     transformIgnorePatterns: [
-        'node_modules/(?!(lit-element|lit-html|lit|@lit/|date-fns|@mdi)/)'
+        'node_modules/(?!(lit-element|lit-html|lit|@lit/|date-fns|@mdi|@material)/)'
     ],
     extensionsToTreatAsEsm: ['.ts'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
